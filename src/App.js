11f42255044499874df6d0a78dc75b10ba951cd0
cutetory <101031776+cutetory@users.noreply.github.com>
2022-08-25 09:47:27 +0900

    new Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  /* 2022-08-25  
+    component아니고 element로 바꿔야하고 Routes 추가해야함.
+    https://reactrouter.com/en/v6.3.0/api#routes 
+  */
+    <HashRouter>
+      <Routes>
+        <Route path="/home" exact={true} element={<Home />} />
+        <Route path="/home/introduction" exact={true} element={<h1>introduction</h1>} />
+        <Route path="/about" exact={true} element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
